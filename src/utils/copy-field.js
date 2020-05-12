dce4e2ae78b62fee2ccc1e79a0e5d9bf442429e6
Mat Janson Blanchet <mat.jb@jansensan.net>
2020-05-11 22:01:20 -0400

    Improved book details
    Signed-off-by: Mat Janson Blanchet <mat.jb@jansensan.net>

---
+++ src/utils/copy-field.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+
+module.exports = copyField;
+
+
+function copyField(source, target) {
+  const json = JSON.stringify(source);
+  const copy = JSON.parse(json);
+  return [_.merge(copy[0], target)];
+}
