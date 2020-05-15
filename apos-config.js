40df13252976b66a005a0290132c1d3a9e42e15a
Mat Janson Blanchet <mat.jb@jansensan.net>
2020-05-15 08:42:19 -0400

    Added basic search feature
    Signed-off-by: Mat Janson Blanchet <mat.jb@jansensan.net>

--- apos-config.js
+++ apos-config.js
@@ -51,6 +51,52 @@ const aposOptions = {
 
     'apostrophe-second-chance-login': {},
 
+    'apostrophe-search': {
+      filters: [
+        // content
+        {
+          name: 'aem-articles',
+          label: 'News'
+        },
+        {
+          name: 'aem-events',
+          label: 'Events'
+        },
+        {
+          name: 'aem-projects',
+          label: 'Projects'
+        },
+        // media
+        {
+          name: 'aem-media',
+          label: 'Medias'
+        },
+        {
+          name: 'aem-books',
+          label: 'Books'
+        },
+        // people and orgs
+        {
+          name: 'aem-persons',
+          label: 'People'
+        },
+        {
+          name: 'aem-organizations',
+          label: 'Organizations'
+        },
+        // education
+        {
+          name: 'aem-education',
+          label: 'Education'
+        },
+        // other
+        {
+          name: '__else',
+          label: 'Pages'
+        }
+      ]
+    },
+
     ...customModules
   }
 };
