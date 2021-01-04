96ee58977d297b561bcb3cd93b6eabd45d7ae453
Mat Janson Blanchet <mat.jb@jansensan.net>
2021-01-04 17:25:01 -0500

    Moved search filters to external file

--- apos-config.js
+++ apos-config.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const fs = require('fs');
+const aposSearchFilters = require('./lib/constants/apos-search-filters');
 
 
 const customModuleNames = fs
@@ -52,49 +53,7 @@ const aposOptions = {
     'apostrophe-second-chance-login': {},
 
     'apostrophe-search': {
-      filters: [
-        // content
-        {
-          name: 'aem-articles',
-          label: 'News'
-        },
-        {
-          name: 'aem-events',
-          label: 'Events'
-        },
-        {
-          name: 'aem-projects',
-          label: 'Projects'
-        },
-        // media
-        {
-          name: 'aem-media',
-          label: 'Medias'
-        },
-        {
-          name: 'aem-books',
-          label: 'Books'
-        },
-        // people and orgs
-        {
-          name: 'aem-persons',
-          label: 'People'
-        },
-        {
-          name: 'aem-organizations',
-          label: 'Organizations'
-        },
-        // education
-        {
-          name: 'aem-education',
-          label: 'Education'
-        },
-        // other
-        {
-          name: '__else',
-          label: 'Pages'
-        }
-      ]
+      filters: aposSearchFilters
     },
 
     ...customModules
